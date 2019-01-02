21086fb9e85c359650a49fe9eeb29065066f74ed
Kevin Kim <kevinerickim@gmail.com>
2019-01-02 18:04:17 -0500

    Separate listing image into separate component

--- client/src/components/listingDesc.jsx
+++ client/src/components/listingDesc.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 
+import ListingImg from './listingImg.jsx';
+
 class ListingDesc extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   imageLink:
-    //     'https://i.pinimg.com/originals/5f/d9/78/5fd9786dc347122dfc4b7ceaed697a0a.jpg',
-    //   listingName: 'Shack Reactor',
-    //   propertyType: 'Restaurant/School lol',
-    //   beds: 13,
-    //   price: 'over 9000',
-    //   rating: '5/7', // TODO: how am i going to convert this to stars?
-    //   numberOfReviews: 314
-    // };
   }
-
-  // when the component mounts, make a request to the correct endpoint for a listing's data
 
   render() {
     return (
       <div>
-        <img
-          className="listingImg"
-          style={{ 'height': 200, 'width': 300, 'borderRadius': '7px'}}
-          src={this.props.listing.imageLink}
-        />
+        <ListingImg imageLink={this.props.listing.imageLink} />
         <div className="listingName">
           Listing Name: {this.props.listing.listingName}
         </div>
