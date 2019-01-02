a5f7ff547c728b4692745b4ff974a514d2ba3886
Kevin Kim <kevinerickim@gmail.com>
2019-01-02 11:52:00 -0500

    Implement native slick arrows as Chevron icons

--- client/dist/app.js
+++ client/dist/app.js
@@ -5,42 +5,6 @@ $(document).ready(() => {
     infinite: false,
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next')
+    arrows: true
   });
 });
-  
-
-/* Native Bootstrap Carousel
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: false
-});
-
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function() {
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next
-    .children(':first-child')
-    .clone()
-    .appendTo($(this));
-
-  if (next.next().length > 0) {
-    next
-      .next()
-      .children(':first-child')
-      .clone()
-      .appendTo($(this));
-  } else {
-    $(this)
-      .siblings(':first')
-      .children(':first-child')
-      .clone()
-      .appendTo($(this));
-  }
-});
-*/
