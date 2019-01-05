c0b0fae7fabdc0d7ca3a8b9c62d893ec02fd4e04
Kevin Kim <kevinerickim@gmail.com>
2019-01-05 09:49:47 -0500

    Server send bundle.js file to empty endpoints

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ let app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(Path.join(__dirname, '../client/dist')));
+app.use(express.static(Path.join(__dirname, '../client/dist/bundle.js')));
 
 app.get('/similar-listings/:listingId', (req, res) => {
   let listingId = req.params.listingId;
