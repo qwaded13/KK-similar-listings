fe4cc9ced05e622be45ee06f8835e546a93d4363
Kevin Kim <kevinerickim@gmail.com>
2019-04-29 20:22:53 -0400

    Images fetch new compressed jpg images

--- client/src/components/listingImg.jsx
+++ client/src/components/listingImg.jsx
@@ -9,10 +9,10 @@ class ListingImg extends React.Component {
           style={{ height: 225, width: 325, borderRadius: '5px' }}
           src={
             this.props.listingId % 2
-              ? `https://s3.us-east-2.amazonaws.com/similar-listings-home-images/test(${this
-                  .props.listingId % 26 || 1}).webp`
-              : `https://s3.us-east-2.amazonaws.com/similar-listings-home-images-alt/test(${this
-                  .props.listingId % 26 || 1}).webp`
+              ? `https://s3.us-east-2.amazonaws.com/similar-listings-home-images/house(${this
+                  .props.listingId % 26 || 1}).jpg`
+              : `https://s3.us-east-2.amazonaws.com/similar-listings-home-images-alt/house(${this
+                  .props.listingId % 26 || 1}).jpg`
           }
         />
       </div>
